85e3330f941bb65e193d6a24d1f5e8e623a7a6ef
Keisuke2162 <applabo.kei@gmail.com>
2020-03-22 17:42:39 +0900

    Initial Commit

---
+++ FontViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FontViewer
+//
+//  Created by 植田圭祐 on 2020/03/22.
+//  Copyright © 2020 Keisuke Ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
